8cffffae498f4eb68a20fbf79584b6dfb766c38b
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2023-01-29 02:24:15 +0300

    tasks 1, 3 fixed

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,30 @@ import java.time.LocalDate;
 
 public class Main {
     public static int currentYear = LocalDate.now().getYear();
+
     public static void main(String[] args) {
+        // 1
         int year = currentYear;
-        int deliveryDistance = 95;
         isLeapYear(year);
+        // 2
         determinateAppVersion(0, currentYear);
-        System.out.println("Потребуется дней для доставки: " + getDeliveryTime(deliveryDistance));
+        // 3
+        int deliveryDistance = 95;
+        if (deliveryDistance <= 100) {
+            System.out.println("Потребуется дней для доставки: " + getDeliveryTime(deliveryDistance));
+        } else {
+            System.out.println("Доставка только в пределах 100 км!");
+        }
     }
+    // 1
     public static void isLeapYear(int year) {
-        if (year % 4 == 0) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " год является високосным!");
-        }
-        else {
-            System.out.println("Год не является високосным!");
+        } else {
+            System.out.println(year + " год не является високосным!");
         }
     }
+    // 2
     public static void determinateAppVersion(int systemType, int yearOfIssue) {
         if (systemType == 1 && yearOfIssue < currentYear) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
@@ -29,7 +38,7 @@ public class Main {
             System.out.println("Установите мобильное приложения для IOS по ссылке");
         }
     }
-
+    // 3
     public static int getDeliveryTime(int distance) {
         int deliveryTime = 0;
         if (distance <= 20) {
